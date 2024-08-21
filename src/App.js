dd373bf1ada89397d2791264045c2846339caacd
musa-sohail <musabinsohail567@gmail.com>
2024-08-22 03:19:53 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -17,18 +17,14 @@ const App = () => (
             <Route exact path="/">
               <Homepage />
             </Route>
-            <Route exact path="/exchanges">
-              <Exchanges />
-            </Route>
+
             <Route exact path="/cryptocurrencies">
               <Cryptocurrencies />
             </Route>
             <Route exact path="/crypto/:coinId">
               <CryptoDetails />
             </Route>
-            <Route exact path="/news">
-              <News />
-            </Route>
+         
           </Switch>
         </div>
       </Layout>
